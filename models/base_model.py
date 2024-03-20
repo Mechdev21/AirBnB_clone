9eedd4617a2a31af3d62966c366ab9555edd68dd
Favour <135041430+favourhut@users.noreply.github.com>
2024-03-20 21:39:26 +0100

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -16,13 +16,13 @@ class BaseModel:
 
     Attributes:
         id: assign with an uuid when an instance is created
-        created_at: assign with the current datetime when an instance is created
-        updated_at: assign with the current datetime when an instance is created and updated
-        
+        created_at: assign with the current datetime when an 
+        instance is created
+        updated_at: assign with the current datetime when an 
+        instance is created and updated
     Args:
         *args: Gets's a number of undisclosed arguments
         **kwargs: passes variable-length argument dictionary to a function
-
     '''       
     def __init__(self, *args, **kwargs):
         """Create BaseModel from dictionary"""
@@ -30,22 +30,24 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["create_at"] = datetime.striptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["create_at"] = datetime.striptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "update_at":
-                    self.__dict__["updated_at"] = datetime.striptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["updated_at"] = datetime.striptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
-                    
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            
+              
     '''Public instance attributes'''
     def __str__(self):
 
         '''Output [<class name>] (<self.id>) <self.__dict__>'''
-        return ("[{}] ({}) {}". format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}". format(
+            self.__class__.__name__, self.id, self.__dict__))
 
 
     """creating the public instances"""
@@ -65,7 +67,9 @@ class BaseModel:
 
         instance_dict = {}
         instance_dict.update(self.__dict__)
-        instance_dict["created_at"] = instance_dict["created_at"].isoformat()
-        instance_dict["updated_at"] = instance_dict["updated_at"].isoformat()
+        instance_dict["created_at"] = instance_dict[
+            "created_at"].isoformat()
+        instance_dict["updated_at"] = instance_dict[
+            "updated_at"].isoformat()
         instance_dict["__class__"] = self.__class__.__name__
         return instance_dict
