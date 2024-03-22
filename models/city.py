10011d3003fd86602076c067d4cd715d82316df6
Favour James <favourjames061@gmail.com>
2024-03-22 15:39:03 -0700

    BaseModels and more classes

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from models.base_models.py import BaseModel
+from models.base_model import BaseModel
 """
 This modue imports from BaseModel
 """
--- models/state.py
+++ models/state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-from models.base_models import BaseModel
+from models.base_model import BaseModel
 """
-Thsi class inherit from BaseModel
+This class inherit from BaseModel
 """
 
 
