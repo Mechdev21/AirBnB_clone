d0a0a012f3eaa0352961886b0f8ee68de0d07af8
Favour James <favourjames061@gmail.com>
2024-03-23 13:16:25 -0700

    python_test.py

---
+++ python_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import cmd
+
+"""
+Writing a code that passed the python code check
+"""
+
+
+class Shell_sample(cmd.Cmd):
+
+    '''next is to create a subclass of the cmd'''
+    def do_hello(self, arg):
+        '''say hello'''
+        print('Hello, World')
+
+    '''exit option subclass'''
+    def do_exit(self, arg):
+
+        '''To exit'''
+        return True
+
+
+'''calling the function'''
+if __name__ == '__main__':
+    display = Shell_sample()
+    '''looping the command line'''
+    display.cmdloop('Input Hello!')
