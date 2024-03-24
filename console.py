fc65322d9dd2a8a24491d9fab571f8cd31077d45
favour orji <favour@titanname.com>
2024-03-24 11:27:47 -0700

    update

--- console.py
+++ console.py
@@ -27,9 +27,106 @@ class HBNBCommand(cmd.Cmd):
         try:
             class_name = classes[args[0]]
             obj = class_name()
+            obj.save()
             print(obj.id)
         except Exception as e:
             print(f"Error creating object: {str(e)}")
+
+    def do_show(self, line):
+        """Show details of a specific instance"""
+        classes = {"BaseModel": BaseModel, "User": User}
+        args = shlex.split(line)
+        instant = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1 and args[0] in classes:
+            print("** instance id missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            if key not in instant:
+                print("** no instance found **")
+            else:
+                print(instant[key])
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        classes = {"BaseModel": BaseModel, "User": User}
+        args = shlex.split(line)
+        instant = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1 and args[0] in classes:
+            print("** instance id missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            if key not in instant:
+                print("** no instance found **")
+            else:
+                del instant[key]
+
+    def do_all(self, line):
+        """Prints all string representation of all instances based"""
+        classes = {"BaseModel": BaseModel, "User": User}
+        args = shlex.split(line)
+        instant = storage.all()
+        if len(args) == 0:
+            print([str(i) for i in instant.values()])
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            if len(args) == 1:
+                instances = [str(v) for v in instant.values() if v.__class__.__name__ == args[0]]
+            print(instances)
+
+    def do_update(self, line):
+        """Updates an instance based on the class name"""
+        classes = {"BaseModel": BaseModel, "User": User}
+        args = shlex.split(line)
+
+        if len(args) < 4:
+            print("Usage: update <class name> <id> <attribute name> <attribute value>")
+            return
+
+        class_name = args[0]
+
+        if class_name not in classes:
+            print("** Class doesn't exist **")
+            return
+
+        if len(args) < 4:
+            print("** Missing arguments **")
+            return
+
+        instance_id = args[1]
+        storage_key = f"{class_name}.{instance_id}"
+        all_instances = storage.all()
+
+        if storage_key not in all_instances:
+            print("** No instance found with provided ID **")
+            return
+
+        instance = all_instances[storage_key]
+        attr_k = args[2]
+        attr_v = args[3]
+
+        try:
+            if attr_v.isdigit():
+                attr_v = int(attr_v)
+            else:
+                attr_v = float(attr_v)
+        except ValueError:
+            pass
+
+        if hasattr(instance, attr_k):
+            setattr(instance, attr_k, attr_v)
+            storage.save()
+            print("Instance updated successfully")
+        else:
+            print("** Attribute name not found **")
 
     def do_quit(self, line):
         """EXIT the cmdloop"""
